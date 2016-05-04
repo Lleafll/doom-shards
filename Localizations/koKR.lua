0df82bdf36030397309e713360d4ce86646aa573
Lleafll <lars.ll.lorenz@gmail.com>
2016-05-04 14:27:12 +0200

    Revert "Merge branch 'test/nopowerdelay'"
    This reverts commit dee93f26b486b1b095f21dd9ab748e7bfcd9ceee, reversing
    changes made to e7262d93936da1f6d7931954854e28060dbd6b67.

---
+++ Localizations/koKR.lua
@@ -0,0 +1,75 @@
+-- Libraries
+local L = LibStub("AceLocale-3.0"):NewLocale("DoomShards", "koKR", false)
+if not L then return end
+
+
+-- Translations
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
+
+--@do-not-package@
+
+L["Aggressive Caching"] = "Aggressive Caching" -- Requires localization
+L["Aggressive Caching Interval"] = "Aggressive Caching Interval" -- Requires localization
+L["Color 1"] = "색상 1"
+L["Color 2"] = "색상 2"
+L["Color 3"] = "색상 3"
+L["Complex"] = "복합"
+L["Complex Display"] = "복합 디스플레이"
+L["Conspicuous Spirits locked!"] = "Conspicuous Spirits 잠금!"
+L["Conspicuous Spirits reset!"] = "Conspicuous Spirits 초기화!"
+L["Conspicuous Spirits unlocked!"] = "Conspicuous Spirits 잠금해제!"
+L["Display"] = "디스플레이"
+L["Display Type"] = "디스플레이 형식"
+L["Enables frequent distance scanning of all available targets. Will increase CPU usage slightly and is only going to increase accuracy in situations with many fast-moving mobs."] = "Enables frequent distance scanning of all available targets. Will increase CPU usage slightly and is only going to increase accuracy in situations with many fast-moving mobs." -- Requires localization
+L["File to play."] = "재생할 파일."
+L["Font"] = "글꼴"
+L["Font Size"] = "글꼴 크기"
+L["General"] = "General" -- Requires localization
+L["Height"] = "높이"
+L["IronMaidensShipMessage"] = "주 대포를 쏠 준비를 합니다!" -- Needs review
+L["Left mouse button to drag."] = "마우스 왼쪽 버튼으로 드래그하세요."
+L["Not possible to unlock in WeakAuras mode!"] = "WeakAuras 모드에서는 잠금해제 할 수 없습니다!"
+L["Open Configuration Window"] = "설정 창 열기"
+L["Orbs"] = "구슬"
+L["Play Warning Sound when about to cap Shadow Orbs."] = "어둠의 구슬이 가득 차면 경고 소리 재생하기."
+L["Position"] = "위치"
+L["Regulate display visibility with macro conditionals"] = "매크로 상태로 디스플레이 표시를 조절하세요"
+L["Reset"] = "초기화"
+L["Reset Position"] = "위치 초기화"
+L["Reset to Defaults"] = "기본으로 초기화"
+L["Scale"] = "크기"
+L["Scanning interval when Aggressive Caching is enabled"] = "Scanning interval when Aggressive Caching is enabled" -- Requires localization
+L["Select font for the Complex display"] = "복합 디스플레이에 사용할 글꼴 선택"
+L["Select font for the Simple display"] = "심플 디스플레이에 사용할 글꼴 선택"
+L["Set Color 1"] = "색상 1 설정"
+L["Set Color 2"] = "색상 2 설정"
+L["Set Color 3"] = "색상 3 설정"
+L["Set Font Size"] = "글꼴 크기 설정"
+L["Set Frame Height"] = "프레임 높이 설정"
+L["Set Frame Scale"] = "프레임 크기 설정"
+L["Set Frame Width"] = "프레임 너비 설정"
+L["Set Number Spacing"] = "간격 설정"
+L["Set Shadow Orb Height"] = "어둠의 구슬 높이 설정"
+L["Set Shadow Orb Spacing"] = "어둠의 구슬 간격 설정"
+L["Set Shadow Orb Width"] = "어둠의 구슬 너비 설정"
+L["Show Orbs out of combat"] = "비 전투 중 어둠의 구슬"
+L["Shows the frame and toggles it for repositioning."] = "위치 변경을 위해 프레임을 표시합니다."
+L["Simple"] = "심플"
+L["Simple Display"] = "심플 디스플레이"
+L["Sound"] = "소리"
+L["Spacing"] = "간격"
+L["Text"] = "문자"
+L["Toggle Lock"] = "잠금 전환"
+L["Visibility"] = "표시"
+L["Warning Sound"] = "소리 경고"
+L["WeakAuras"] = "WeakAuras"
+L["WeakAuras String"] = "WeakAuras 구문"
+L["WeakAuras String to use when \"WeakAuras\" Display is selected. Copy & paste into WeakAuras to import."] = "WeakAuras 구문은 \"WeakAuras\" 디스플레이를 선택 했을때 사용됩니다. WeakAuras 가져오기에 복사 & 붙여넣기 하세요."
+L["Width"] = "너비"
+L["Will show Shadow Orbs frame even when not in combat."] = "전투 중이 아닐 때 프레임에 어둠의 구슬이 표시됩니다."
+L["X Offset"] = "X 좌표"
+L["X offset for the Shadowy Apparition time text"] = "상서로운 영혼 시간 문자의 X 좌표"
+L["Y Offset"] = "Y 좌표"
+L["Y offset for the Shadowy Apparition time text"] = "상서로운 영혼 시간 문자의 Y 좌표"
+
+--@end-do-not-package@
