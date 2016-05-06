b8a43134a289e8cdf3a399bfbafc24ae80d6c90d
Lleafll <lars.ll.lorenz@gmail.com>
2016-05-06 10:24:41 +0200

    Added file for Korean localization

---
+++ Localizations/koKR.lua
@@ -0,0 +1,11 @@
+-- Libraries
+local L = LibStub("AceLocale-3.0"):NewLocale("DoomShards", "koKR", false)
+if not L then return end
+
+
+-- Translations
+--@localization(locale="koKR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="none")@
+
+--@do-not-package@
+
+--@end-do-not-package@
